82c6422f643465ff343d7ad39a277541a14da50d
Ryan Yu <ryanyu@Ryans-MacBook-Pro.local>
2014-07-20 14:55:23 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Framework Application
+
+This is a framework application that contains ______
+by [Ryan Yu](http://ryan-yu.com/).
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FrameworkApp::Application.config.session_store :cookie_store, key: '_framework_app_session'
